592d1df7342e9454e44bd69b8a2880fb0b8e812f
hobzz-droid <lawalobalolu@gmail.com>
2026-07-15 20:03:45 +0100

    add project source code

---
+++ main.py
@@ -0,0 +1,9 @@
+name1 = "micah"
+name2 = "lolade"
+print("hey")
+print("hi")
+print("how are you doing")
+print("i'm fine")
+print("my name is", name2)
+print("what's your name")
+print("my name is", name1)
